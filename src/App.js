f79cbe70adb6f6565365acf52652086b716ae962
ar[arnau321 <arnau123@att.net>
2021-10-08 10:07:48 -0400

    Add css

--- src/App.js
+++ src/App.js
@@ -10,34 +10,40 @@ function App() {
   // state information
   const [userName, setUserName] = useState('')
   const [room, setRoom] = useState('')
+  const [showChat, setShowChat] = useState(false)
 
   // function to join room
   const joinRoom = () => {
     if(userName !== '' && room !== ''){
       socket.emit('join_room', room)
-      
+      setShowChat(true)
     }
   }
 
   return (
     <div className="App">
-      <h3>Join a ChatRoom</h3>
-      <input 
-        type='text' 
-        placeholder='User...' 
-        onChange={(event) => {
-          setUserName(event.target.value)
-        }}
-      />
-      <input
-        type='text' 
-        placeholder='Room'
-        onChange={(event) => {
-          setRoom(event.target.value)
-        }}
-        />
-      <button onClick={joinRoom}>join a room</button>
-      <Chat socket={socket} userName={userName} room={room}/>
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat Room</h3>
+          <input
+            type="text"
+            placeholder="User..."
+            onChange={(event) => {
+              setUserName(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={userName} room={room} />
+      )}
     </div>
   )
 }
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, {useEffect, useState } from 'react'
+import ScrollToBottom from 'react-scroll-to-bottom'
 
 function Chat({socket, userName, room}) {
-  // state for message
+  // state for message  and array of messages
   const [currentMessage, setCurrentMessage] = useState('')
+  const [messageList, setMessageList] = useState([])
 
   // Sends message with socket
   const sendMessage = async () => {
@@ -19,26 +20,58 @@ function Chat({socket, userName, room}) {
 
       }
     await socket.emit('send_message', messageData)
+    setMessageList((list) => [...list, messageData])
+    setCurrentMessage('')
     }
   }
+  // listen for change in socket server from backend 
   useEffect(() => {
     socket.on('receive_message', (data) => {
-      console.log('data', data)
+      console.log('data from server', data)
+      // adds data to list array
+      setMessageList((list) => [...list, data])
     })
   }, [socket])
+
   return (
-    <div>
-      <div className='chat-header'></div>
-        <p>live chat</p>
-      <div className='chat-body'></div>
-      <div className='chat-footer'>
-        <input 
-          type='text'
-          placeholder='message...'
+    <div className="chat-window">
+      <div className="chat-header">
+        <p>Live Chat</p>
+      </div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent) => {
+            return (
+              <div
+                className="message"
+                id={userName === messageContent.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          value={currentMessage}
+          placeholder="Hey..."
           onChange={(event) => {
-            setCurrentMessage(event.target.value)
+            setCurrentMessage(event.target.value);
           }}
-          />
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
+          }}
+        />
         <button onClick={sendMessage}>&#9658;</button>
       </div>
     </div>
